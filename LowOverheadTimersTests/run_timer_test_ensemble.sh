c8406fab667cdaac949d4f6a42a3dd314a9d7d28
John D. McCalpin <mccalpin@tacc.utexas.edu>
2018-07-10 13:02:01 -0500

    Updated run script to skip executables that don't exist.

--- LowOverheadTimersTests/run_timer_test_ensemble.sh
+++ LowOverheadTimersTests/run_timer_test_ensemble.sh
@@ -10,11 +10,16 @@ do
 		echo "Running $NTRIALS iterations of timer_ovhd_${MODE}.${COMPILER}.exe"
 		rm -f log.test_${MODE}_${COMPILER}
 
-		for ITER in `seq 0 $MAXTRIAL`
-		do
-			echo -n "$ITER "
-			./timer_ovhd_${MODE}.${COMPILER}.exe >> log.test_${MODE}_${COMPILER}
-		done
-		echo ""
+		if [ -x ./timer_ovhd_${MODE}.${COMPILER}.exe ]
+		then
+			for ITER in `seq 0 $MAXTRIAL`
+			do
+				echo -n "$ITER "
+				./timer_ovhd_${MODE}.${COMPILER}.exe >> log.test_${MODE}_${COMPILER}
+			done
+			echo ""
+		else
+			echo "executable ./timer_ovhd_${MODE}.${COMPILER}.exe not found, skipping...."
+		fi
 	done
 done
