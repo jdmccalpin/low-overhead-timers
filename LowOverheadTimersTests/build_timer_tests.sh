96ee0f4d9c49d35c84cba3bd3c1a15bb66751110
John D. McCalpin <mccalpin@tacc.utexas.edu>
2018-07-10 13:00:06 -0500

    Updated build script to skip icc build if compiler not found.

--- LowOverheadTimersTests/build_timer_tests.sh
+++ LowOverheadTimersTests/build_timer_tests.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-echo "Building for icc"
-echo "  IMPORTANT NOTES:"
-echo "    1. Choose a target architecture that does not include 256-bit or 512-bit SIMD support"
-echo "       (Any SSE target will do -- the default for Linux is -msse2"
-echo "    2. The compiler flag -nolib-inline prevents the compiler from generating calls to"
-echo "       memset() or memcpy() that might contain 256-bit or 512-bit instructions."
-echo " "
-echo "Building for gcc"
-echo "    1. Choose a target architecture that does not include 256-bit or 512-bit SIMD support"
-echo "       (Any SSE target will do -- gcc defaults to -msse or -msse2.)"
-echo "    2. The flag -fno-tree-loop-distribute-patterns will prevent the compiler from"
-echo "       generating most calls to memset() or memcpy() (which might include 256-bit"
-echo "       or 512-bit instructions."
-echo " "
+which icc >& /dev/null
+if [ $? -ne 0 ]
+then
+    echo "Intel icc compiler not found, skipping...."
+else
+	echo "Intel icc compiler found...."
+	echo "compiling externally linked version with icc"
+	icc --version
+	icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -c ../low_overhead_timers.c -o low_overhead_timers_icc.o
+	icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -I.. test_timer_overhead.c low_overhead_timers_icc.o -qopt-report=5 -o timer_ovhd_external.icc.exe
+	mv test_timer_overhead.optrpt test_timer_overhead.external.optrpt
+	icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -I.. test_timer_overhead.c -S -o timer_ovhd_external.icc.s
 
-echo "compiling externally linked version with icc"
-icc --version
-icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -c ../low_overhead_timers.c -o low_overhead_timers_icc.o
-icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -I.. test_timer_overhead.c low_overhead_timers_icc.o -qopt-report=5 -o timer_ovhd_external.icc.exe
-mv test_timer_overhead.optrpt test_timer_overhead.external.optrpt
-icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -I.. test_timer_overhead.c -S -o timer_ovhd_external.icc.s
-
-echo "compiling inlined version with icc"
-icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -DINLINE_TIMERS -I.. test_timer_overhead.c -qopt-report=5 -o timer_ovhd_inline.icc.exe
-mv test_timer_overhead.optrpt test_timer_overhead.inline.optrpt
-icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -DINLINE_TIMERS -I.. test_timer_overhead.c -S -o timer_ovhd_inline.icc.s
-
+	echo "compiling inlined version with icc"
+	icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -DINLINE_TIMERS -I.. test_timer_overhead.c -qopt-report=5 -o timer_ovhd_inline.icc.exe
+	mv test_timer_overhead.optrpt test_timer_overhead.inline.optrpt
+	icc -O2 -msse2 -nolib-inline -DUSE_PAUSE -DINLINE_TIMERS -I.. test_timer_overhead.c -S -o timer_ovhd_inline.icc.s
+fi
 
 echo "compiling externally linked version with gcc"
 gcc --version
